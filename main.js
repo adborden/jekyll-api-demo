3abcc531f5cb789059b1910b162c2db3dcf479ad
Aaron D Borden <adborden@a14n.net>
2017-11-13 14:50:45 -0800

    wip

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 (function ($) {
   $(document).ready(function () {
+    var $jsFruit = $('#js-fruit');
+    if (!$jsFruit.length) {
+      return;
+    }
+
     var fruits = ['apple', 'banana', 'orange'];
 
     // Random fruit
     var fruit = fruits[Math.floor(Math.random() * fruits.length)];
 
-    $.ajax('window.API_URL + '/fruits/' + fruit + '.json')
+    $.ajax(window.API_URL + '/fruits/' + fruit + '.json')
       .then(function (fruit) {
-        $('#js-fruit')
+        $jsFruit
           .append($('<pre></pre><br>').text(JSON.stringify(fruit, null, 2)))
           .append(fruit.description);
       });
